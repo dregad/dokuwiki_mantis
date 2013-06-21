184fbe68df0d5de9c2cd9056f714846182d30696
Damien Regad <dregad@mantisbt.org>
2013-06-21 18:47:30 +0200

    Auth backend: simplify code
    Move code to retrieve user info to a private function, called from
    trustExternal() and getUserData().

--- inc/auth/mantis.class.php
+++ inc/auth/mantis.class.php
@@ -8,7 +8,6 @@
  * added modifications to make possible to set project related user rights.(http://www.mantisbt.org/bugs/view.php?id=8249)
  * added modifications so that mantis support logging in/out from DokuWiki's login page (http://www.mantisbt.org/bugs/view.php?id=8277)
  *
- *
  * @author    Victor Boctor (http://www.futureware.biz)
  *
  */
@@ -17,98 +16,77 @@ require_once( MANTIS_ROOT . 'core.php' );
 #dbg($GLOBALS);
 
 class auth_mantis extends auth_basic {
+
 	/**
 	 * Constructor.
 	 *
 	 * Sets additional capabilities and config strings
 	 */
-	function auth_mantis(){
+	function auth_mantis() {
 		$this->cando['external'] = true;
-
-		$this->cando['logoff' ] = true; // module has a logoff method
+		$this->cando['logoff'] = true;
 	}
 
 	/**
 	 * Authenticates the user using Mantis APIs.
+	 * @param   string  $user    Username
+	 * @param   string  $pass    Cleartext Password
+	 * @param   bool    $sticky  Cookie should not expire
+	 * @return  bool             true on successful auth
 	 */
-	function trustExternal($user,$pass,$sticky=false){
+	function trustExternal( $user, $pass, $sticky = false ) {
 		global $USERINFO;
 		global $conf;
 
 		$ValidUser = false;
 
 		// Manage HTTP authentication with Negotiate protocol enabled
-		$user = auth_prepare_username($user);
-	$pass = auth_prepare_password($pass);
+		$user = auth_prepare_username( $user );
+		$pass = auth_prepare_password( $pass );
+
 		// This is necessary in all cases where Authorization HTTP header is always set
-	if(auth_is_user_authenticated())
-	{
-	    $user='';
-	}
+		if( auth_is_user_authenticated() ) {
+			$user = '';
+		}
 
 		// Has a user name been provided?
-		if ( !empty ( $user ) )
-		{
+		if( !empty( $user ) ) {
 			// User name provided, so login via form in progress...
 			// Are the specified user name and password valid?
-			if ( auth_attempt_login ( $user, $pass, $sticky ) )
-			{
+			if( auth_attempt_login( $user, $pass, $sticky ) ) {
 				// Credential accepted...
 				$_SERVER['REMOTE_USER'] = $user; // Set the user name (makes things work...)
 				$ValidUser = true; // Report success.
 			}
-			else
-			{
+			else {
 				// Invalid credentials
-						if ( !$silent )
-				{
-					msg ( $lang [ 'badlogin' ], -1 );
+				if( !$silent ) {
+					msg( $lang [ 'badlogin' ], -1 );
 				}
 
 				$ValidUser = false;
 			}
 		}
-		else
-		{
+		else {
 			// No user name provided.
 			// Is a user already logged in?
-			if ( auth_is_user_authenticated ( ) )
-			{
+			if(  auth_is_user_authenticated() ) {
 				// Yes, a user is logged in, so set the globals...
-				// is it a media display or a page?
-				if (isset($_REQUEST['media'])) {
-					//media
-					$t_project_name = explode( ':', getNS( getID("media",false) ) );
-				} else {
-					// normal page
-					$t_project_name = explode( ':', getNS( getID() ) );
-				}
-				$t_project_id = project_get_id_by_name( $t_project_name[1] );
-				$t_access_level = access_get_project_level( $t_project_id );
-				$t_access_level_string = strtoupper( MantisEnum::getLabel( config_get( 'access_levels_enum_string' ),  $t_access_level ) ); // mantis 1.2.0rc
-				// $t_access_level_string = strtoupper( get_enum_to_string( config_get( 'access_levels_enum_string' ),  $t_access_level ) );
-				$t_access_level_string_ex = strtoupper( $t_project_name[1] ) . '_' . $t_access_level_string;
 
-				$USERINFO['grps'] = array( $t_access_level_string, $t_access_level_string_ex );
-				$USERINFO[ 'pass' ] = current_user_get_field ( 'password' );
-				$USERINFO[ 'name' ] = current_user_get_field ( 'username' );
-				$USERINFO[ 'mail' ] = current_user_get_field ( 'email' );
+				$t_user_id = auth_get_current_user_id();
 
-				$_SERVER[ 'REMOTE_USER' ] = $USERINFO[ 'name' ];
-				$_SESSION[ $conf[ 'title' ]][ 'auth' ][ 'user' ] = $USERINFO[ 'name' ];
-				$_SESSION[ $conf[ 'title' ]][ 'auth' ][ 'info' ] = $USERINFO;
+				$USERINFO = $this->_loadUserData( $t_user_id );
+				$_SERVER[ 'REMOTE_USER' ] = $USERINFO['name'];
 
 				$ValidUser = true;
 			}
-			else
-			{
+			else {
 				$ValidUser = false;
 			}
 		}
 
 		// Is there a valid user login?
-		if ( true != $ValidUser )
-		{
+		if( !$ValidUser ) {
 			// No, so make sure any existing authentication is revoked.
 			auth_logoff ( );
 		}
@@ -118,40 +96,50 @@ class auth_mantis extends auth_basic {
 	/**
 	 * Logout from Mantis
 	 */
-	function logOff(){
+	function logOff() {
 		auth_logout();
 	}
 
 	/**
-	 * Get user data
-	 * (needed for e-mail subscriptions)
+	 * Return user info
 	 *
-	 * @author Martin Arends http://www.web-gestaltung.de
+	 * @param string $user username
+	 * @return array|false if user does not exist
 	 */
-	function getUserData($user)
-	{
-		$data = array();
+	function getUserData( $user ) {
+		return $this->_loadUserData( user_get_id_by_name( $user ) );
+	}
 
-		$t_user_id = user_get_id_by_name( $user );
+	/**
+	 * Retrieves user information from MantisBT
+	 * @param int $p_user_id
+	 * @return array|false if user does not exist
+	 */
+	private function _loadUserData( $p_user_id ) {
+		if( !user_exists( $p_user_id ) ) {
+			return false;
+		}
 
-		if (isset($_REQUEST['media'])) {
+		// is it a media display or a page?
+		if( isset( $_REQUEST['media'] ) ) {
 			//media
-			$t_project_name = explode( ':', getNS( getID("media",false) ) );
-		} else {
+			$t_project_name = explode( ':', getNS( getID( "media", false ) ) );
+		}
+		else {
 			// normal page
 			$t_project_name = explode( ':', getNS( getID() ) );
 		}
 
-		$t_project_id   = project_get_id_by_name( $t_project_name[1] );
-		$t_access_level = access_get_project_level( $t_project_id, $t_user_id );
-		$t_access_level_string    = strtoupper( MantisEnum::getLabel( config_get( 'access_levels_enum_string' ),  $t_access_level ) );
+		$t_project_id = project_get_id_by_name( $t_project_name[1] );
+		$t_access_level = access_get_project_level( $t_project_id );
+		$t_access_level_string = strtoupper( MantisEnum::getLabel( config_get( 'access_levels_enum_string' ),  $t_access_level ) );
 		$t_access_level_string_ex = strtoupper( $t_project_name[1] ) . '_' . $t_access_level_string;
 
-		$data['name'] = $user;
-		$data['grps'] = array( $t_access_level_string, $t_access_level_string_ex );
-		$data['mail'] = user_get_email( $t_user_id );
-
-		return $data;
+		return array(
+			'name' => user_get_field( $p_user_id, 'username' ),
+			'pass' => user_get_field( $p_user_id, 'password' ),
+			'mail' => user_get_field( $p_user_id, 'email' ),
+			'grps' => array( $t_access_level_string, $t_access_level_string_ex ),
+		);
 	}
 }
-
